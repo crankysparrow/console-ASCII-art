3b9a3e9254f23decdb8bed33bde9c1a032840d4e
Michelle <michelle.en4@gmail.com>
2019-10-10 16:01:20 -0400

    new ASCII string, luminance formula

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var Jimp = require('jimp');
 
-// var ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-// ascii = ascii.split('').reverse().join('');
-
-var ascii = ' .,:;i1tfLCG08';
+// var ascii = " `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+var ascii = " .:-=+*#%@";
 ascii = ascii.split('').reverse().join('');
 
-Jimp.read('cat.jpg', function(err, image) {
+Jimp.read(process.argv[2], function(err, image) {
 
   if (err) console.error(err);
 
@@ -17,16 +15,17 @@ Jimp.read('cat.jpg', function(err, image) {
   let height = image.bitmap.height;
 
   // cycle through each pixel in image, one row at a time
-  for (var y = 0; y < height; y++) {
+  for (var y = 0; y < height; y += 2) {
     let row = '';
     for (var x = 0; x < width; x++) {
 
       let pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
 
       // calculate lightness of each pixel
-      let lightness = calculateLightness(pixel.r, pixel.g, pixel.b);
+      // let lightness = calculateLightness(pixel.r, pixel.g, pixel.b);
+      let measure = calculateLuminance(pixel.r, pixel.g, pixel.b);
       // add each pixel in the form of an ASCII character to the current row
-      row += findAscii(lightness, ascii);
+      row += findAscii(measure, ascii);
     }
 
     // once app has cycled through all pixels in one row, print the row to the console and move on to the next
@@ -35,15 +34,20 @@ Jimp.read('cat.jpg', function(err, image) {
 
 });
 
-// Function for calculating the lightness of a color based on R, G, B values.
-const calculateLightness = (r, g, b) => {
-  r = r/255;
-  g = g/255;
-  b = b/255;
-  return 1/2*(Math.max(r, g, b) + Math.min(r, g, b));
+// Function for calculating the brightness of a color based on R, G, B values.
+const calculateBrightness = (r, g, b) => {
+  return ((r + g + b)/3)/255;
+}
+
+// Function for  calculating the luminance of a pixel based on RGB values.
+const calculateLuminance = (r, g, b) => {
+  return (0.299*r + 0.587*g + 0.114*b)/255;
 }
 
 // Function for returning ASCII character based on lightness of a pixel. Takes a string of characters sorted from darkest to lightest.
-const findAscii = (lightness, ascii) => {
-  return ascii[Math.floor(lightness * ascii.length)];
+const findAscii = (measure, ascii) => {
+  if (measure === 1) {
+    return ascii[ascii.length-1];
+  }
+  return ascii[Math.floor(measure * ascii.length)];
 }
